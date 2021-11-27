9d7d074de5cf201f70ce2245b2e9d3e3bba0e6f8
MauriceM <0980991@hr.nl>
2021-11-27 20:23:58 +0100

    Currently a heap of garbage, might become useful

--- main.py
+++ main.py
@@ -4,25 +4,34 @@ class BeatMaker:
     def __init__(self):
         #####DEFAULT VALUES########
         # Range of notes where 60 = C5
-        self.degrees = [60, 62, 63, 65, 67, 68, 70, 72] # C minor
+        self.degrees  = [60, 62, 63, 65, 67, 68, 70, 72] # C minor
         # Track to which notes are added (Piano roll???)
-        self.track   = 0
+        self.track    = 0
         # MIDI self.channel nr
-        self.channel = 1
+        self.channel  = 1
         # Time at which the note is played in a bar
-        self.time    = 0
+        self.time     = 0
         # Length of note
         self.duration = 1
         # BPM
-        self.tempo = 140
+        self.tempo    = 140
         # Volume 0-127
-        self.volume = 100
-        # Amount of beats per bar
-        self.beats = 4
+        self.volume   = 100
         # Amount of bars per song
-        self.amtbars = 64
+        self.amt_bars = 64
+        # Amount of beats per bar
+        self.beats    = 4
         # Instance of Midi object
-        self.MyMIDI = MIDIFile(1)
+        self.MyMIDI   = MIDIFile(1)
+        # List of all notes in order
+        self.notes = []
+
+    def addToMIDI(self):
+        # track = [[[track, channel, pitch, time, duration, volume], [1], [2], [3]], [4, 5, 6, 7], [8, 9, 10, 11], [12, 13, 14, 15]]
+        
+        for bar_nr in range(self.amt_bars):
+            for note_nr in range(bar_nr): # Time = note number
+                self.MyMIDI.addNote(self.track, self.channel, self.pitch, note_nr, self.duration, self.volume)
 
     def generateDegrees(self, rootdegree, amt, scale):
         if scale == 'major':
@@ -42,7 +51,7 @@ class BeatMaker:
     def generateMelody(self):
         bassdegrees = [self.degrees[0] - 12, self.degrees[4] - 12, self.degrees[3] - 12, self.degrees[5] - 12] # (Semi) Hardcoded bass notes (will adjust to major/minor)
         bassnote = 0
-        for bars in range(self.amtbars):
+        for bars in range(self.amt_bars):
             self.MyMIDI.addNote(self.track, self.channel, bassdegrees[bassnote % 4], self.time, 4, self.volume)
             bassnote += 1
             self.generateBar()
@@ -54,8 +63,30 @@ class BeatMaker:
 
             self.time = self.time + 1
 
+    def generateTriads(self, tonality):
+        if tonality == 'major':
+            diatonic_triads = ['I', 'ii', 'iii', 'IV', 'V', 'vi']
+            triad_intervals = [0, 3, 4]
+        elif tonality == 'minor':
+            diatonic_triads = ['i' , 'ii', 'III', 'iv', 'v', 'VI']
+            triad_intervals = [0, 4, 3]
+        else:
+            print('This tonality has not yet been implemented')
+            return
+
+        progression = []
+        for triads in range(4): # A progression is 4 triads in this case.
+            triad = r.choice(diatonic_triads)
+            while triad in progression: # A progression must have 4 unique triads
+                 triad = r.choice(diatonic_triads)
+            progression.append(triad)
+
+        for time, triad in enumerate(progression):
+            self.time = time * 4 # Multiply by for to spread it out over each bar
+            self.triadToMIDI(triad_intervals, triad)
+
     def setAmountBars(self, amt):
-        self.amtbars = amt
+        self.amt_bars = amt
 
     def setBeatsPerBar(self, amt):
         self.beats = amt
